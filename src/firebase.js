51d131a0d7948a552cd87d6c044dd85a2069c55f
AhmadRafly23 <pradanaahmadrafly@gmail.com>
2023-05-19 01:37:47 +0700

    update

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB6fFWjE4aXtHqOUZezEiMR9pmJFCWgbE4',
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
